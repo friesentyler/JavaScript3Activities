126dbe8926be83881e70062f6c32aafe78f304ae
Tyler <tytyfries@gmail.com>
2025-03-24 13:30:16 -0700

    Activity5 completed

--- MilestoneProject/PersistentMilestone/milestone-angular/src/app/models/Listing.ts
+++ MilestoneProject/PersistentMilestone/milestone-angular/src/app/models/Listing.ts
@@ -1,12 +1,3 @@
-/*export interface Listing {
-  order_id?: number;
-  lister_id: number;
-  artist: string;
-  title: string;
-  record_condition: string;
-  image_location: string;
-}*/
-
 export interface Listing {
   listingId?: number;
   listerId: number;
